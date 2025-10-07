961d645652fbe4b923a5ec4a4f019b17b8ab5eb3
Javier Rodríguez Martínez <i4life.javierrm@gmail.com>
2025-10-07 11:01:12 +0200

    front con autentificacion

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core'; 
+import { Component, inject, OnInit } from '@angular/core'; 
 import { TaskService, Task } from '../services/task.service'; 
 import { IonicModule } from '@ionic/angular'; // <-- Import IonicModule
 import { FormsModule } from '@angular/forms'; // <-- Import FormsModule for [(ngModel)]
 import { CommonModule } from '@angular/common'; // <-- Import CommonModule for *ngFor, etc.
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
  
 @Component({ 
   selector: 'app-home', 
@@ -19,8 +22,12 @@ import { CommonModule } from '@angular/common'; // <-- Import CommonModule for *
 export class HomePage implements OnInit { 
   tasks: Task[] = []; 
   newTaskTitle: string = ''; 
+
+  private readonly apiUrl = 'http://localhost:5000';
+
+  private authService = inject(AuthService);
  
-  constructor(private taskService: TaskService) { } 
+  constructor(private http: HttpClient, private taskService: TaskService) { } 
  
   ngOnInit() { 
     this.loadTasks(); 
@@ -50,4 +57,14 @@ deleteTask(task: Task) {
     this.tasks = this.tasks.filter(t => t._id !== task._id); 
   }); 
 } 
+
+getTasks(): Observable<Task[]> { 
+  const headers = new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('token')}`); 
+  return this.http.get<Task[]>(this.apiUrl, { headers });
+}
+
+logout() { 
+  this.authService.logout(); 
+  }   
+
 }
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular'; // <-- 1. Import IonicModule
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: true, // <-- Confirms it's a standalone component
+  imports: [         // <-- 2. Add imports array
+    CommonModule,
+    FormsModule,
+    IonicModule      // <-- 3. Include IonicModule here
+  ]
+})
+
+export class LoginPage { 
+  username = ''; 
+  password = ''; 
+  constructor(private authService: AuthService, private router: Router) { } 
+  login() { 
+  this.authService.login(this.username, 
+  this.password).subscribe(() => { 
+  this.router.navigate(['/home']);  
+  }); 
+  } 
+  }

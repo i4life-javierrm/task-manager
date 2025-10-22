6ff9d1e5d43d271b81e464a5fff5729efe61189a
Javier Rodríguez Martínez <i4life.javierrm@gmail.com>
2025-10-22 18:01:29 +0200

    service preparado a mano, files creadas

---
+++ src/app/services/notification.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core'; 
+import { HttpClient, HttpParams } from '@angular/common/http'; 
+import { Observable } from 'rxjs'; 
+import { environment } from '../../environments/environment';
+
+export interface Notification {
+    _id?:String,
+    message?:String,
+    user:{username:string,_id:string},
+    task:{title:string,_id:string}
+}
+
+@Injectable({
+    providedIn:'root'
+})
+
+export class NotificationService {
+    private apiUrl = environment.apiUrl+'/notifications'
+
+    constructor (private http:HttpClient){}
+
+    getNotifications(): Observable<Notification[]>
+    {
+        return this.http.get<Notification[]>(this.apiUrl)
+    }
+
+    createNotification(data: {message?:string,user:string,task:string}): Observable<Notification>
+    {
+        return this.http.post<Notification>(this.apiUrl, data)
+    }
+
+    deleteNotification(id:string): Observable<any>
+    {
+        return this.http.delete(`${this.apiUrl}/${id}`)
+    }
+}
